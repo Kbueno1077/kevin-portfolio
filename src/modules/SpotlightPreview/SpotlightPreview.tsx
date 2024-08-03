aa5f82eb6dcf29cb413c43b9b9ade4628e5c4263
Kbueno1077 <kbueno1077@gmail.com>
2024-08-03 17:16:24 -0400

    Spotlight Full Height trick

--- src/modules/SpotlightPreview/SpotlightPreview.tsx
+++ src/modules/SpotlightPreview/SpotlightPreview.tsx
@@ -3,12 +3,12 @@ import { Spotlight } from "@/components/ui/spotlight";
 
 export function SpotlightPreview() {
     return (
-        <div className="h-full w-full rounded-md flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
+        <div className="h-[calc(100dvh-42px)] w-full rounded-md flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
             <Spotlight
                 className="top-20 left-0 md:left-60 md:-top-20"
                 fill="white"
             />
-            <div className="p-4 max-w-7xl mx-auto flex flex-col justify-center md:block relative z-10  w-full pt-20 md:pt-0">
+            <div className=" p-4 max-w-7xl mx-auto flex flex-col justify-center md:block relative z-10  w-full  ">
                 <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text py-2 text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
                     Welcome <br /> to my portfolio!
                 </h1>
