281119e6a71b75e091bf0bec723d4f9ed5db9b92
Kbueno1077 <kbueno1077@gmail.com>
2024-10-20 15:46:33 -0400

    update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,19 +10,56 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
     icons: { icon: "./favicon.ico" },
-    title: "Kevin Bueno's Portfolio",
+    title: "Kevin Bueno's Portfolio - Frontend Developer & UI/UX Specialist",
     keywords: [
-        "Kevin",
         "Kevin Bueno",
+        "Kevin Studio",
         "Portfolio",
         "Web Developer",
+        "Web Applications ",
         "Frontend Developer",
-        "React",
-        "UI/UX",
-        "Projects",
+        "JavaScript",
+        "TypeScript",
+        "React Developer",
+        "Next.js Developer",
+        "Svelte Developer",
+        "SvelteKit Developer",
+        "UI/UX Designer",
+        "Frontend Projects ",
+
+        "Responsive Design",
     ],
     description:
-        "Hi, I’m Kevin, a skilled frontend developer specializing in converting mockups into responsive, user-friendly web applications.",
+        "Welcome to Kevin Bueno's portfolio. Kevin is a skilled frontend developer specializing in converting mockups into responsive, user-friendly web applications using React, Next.js, and modern web technologies.",
+    openGraph: {
+        type: "website",
+        url: "kbueno-studio.com",
+        title: "Kevin Bueno's Portfolio - Frontend Developer & UI/UX Specialist",
+        description:
+            "Explore Kevin Bueno's portfolio showcasing projects and skills in frontend development, UI/UX design, and modern web technologies.",
+        images: [
+            {
+                url: "kbueno-studio.com/og-image.jpg",
+                width: 1200,
+                height: 630,
+                alt: "Kevin Bueno's Portfolio",
+            },
+        ],
+    },
+    twitter: {
+        card: "summary_large_image",
+        site: "@kbueno1077",
+        creator: "@kbueno1077",
+        title: "Kevin Bueno's Portfolio - Frontend Developer & UI/UX Specialist",
+        description:
+            "Discover the projects and skills of Kevin Bueno, a frontend developer specializing in React, Next.js, and UI/UX design.",
+        images: [
+            {
+                url: "kbueno-studio.com/twitter-image.jpg",
+                alt: "Kevin Bueno's Portfolio",
+            },
+        ],
+    },
 };
 
 export const viewport: Viewport = {
